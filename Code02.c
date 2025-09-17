dad91f0a7e01a0f2cb48718e27ac10f725d228f6
Siddaling Hiremath <siddalinghiremath19@email.com>
2025-09-17 21:57:18 +0530

    Born year

---
+++ Code02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int a,b;
+    char Name;
+    printf("what is your age:");
+    scanf("%d",&a);
+    b= 2025-a;
+    printf("you are born in the year:%d\n",b);
+
+    return 0;
+}
