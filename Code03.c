9dcf1d3575f35ee57418dbf7b2fa201a9d3b429a
Siddaling Hiremath <siddalinghiremath19@email.com>
2025-10-20 23:28:26 +0530

    Code03.c

---
+++ Code03.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    char name[15];
+    printf("Enter your name:");
+    scanf("%s",&name);
+    printf("Hi %s.\n Well Come to Coding world",name);
+
+
+    return 0;
+}
